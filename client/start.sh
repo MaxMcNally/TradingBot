a9064de0c8c93dc4520e192f89461314630d15ef
Max McNally <maxmcnally@gmail.com>
2025-10-07 07:01:18 -0700

    Fix nginx permission issues in client Docker container
    - Remove nginx log symlinks that pointed to /dev/stdout and /dev/stderr
    - Create regular log files with proper nginx user ownership
    - Configure nginx to use /var/run/nginx/nginx.pid for PID file
    - Disable nginx logging to prevent permission issues
    - Client Docker build now works successfully locally
    
    Fixes Railway deployment crash: nginx permission denied errors

--- client/start.sh
+++ client/start.sh
@@ -6,9 +6,11 @@ API_URL=${API_URL:-"http://api-qa-qa.up.railway.app"}
 # Replace the API URL in nginx config
 sed -i "s|http://api-qa-qa.up.railway.app|$API_URL|g" /etc/nginx/nginx.conf
 
-# Create nginx pid directory and set permissions
+# Create nginx directories and set permissions
 mkdir -p /var/run/nginx
+mkdir -p /var/log/nginx
 chown -R nginx:nginx /var/run/nginx
+chown -R nginx:nginx /var/log/nginx
 
 # Start nginx as nginx user
 exec su-exec nginx nginx -g 'daemon off;'
