6b91c8ae9ece134e9c096290515e4b435ee11929
Max McNally <maxmcnally@gmail.com>
2025-10-07 13:08:53 -0700

    Fix ESLint error in node-fetch mock file (v2)
    - Move jest.fn() call to separate line to properly apply eslint-disable
    - Extract mockFetch variable to avoid inline jest reference
    - Resolves 'jest is not defined' error on line 4

--- src/__mocks__/node-fetch.js
+++ src/__mocks__/node-fetch.js
@@ -1,11 +1,13 @@
 // Mock for node-fetch to avoid ES module issues in Jest
 // eslint-disable-next-line no-undef
+const mockFetch = jest.fn(() => Promise.resolve({
+  ok: true,
+  status: 200,
+  json: () => Promise.resolve([]),
+  text: () => Promise.resolve(''),
+  headers: new Map()
+}));
+
 module.exports = {
-  default: jest.fn(() => Promise.resolve({
-    ok: true,
-    status: 200,
-    json: () => Promise.resolve([]),
-    text: () => Promise.resolve(''),
-    headers: new Map()
-  }))
+  default: mockFetch
 };
