0abba9921068eb35a345bcdc6e0367629aba3228
Max McNally <maxmcnally@gmail.com>
2025-10-06 22:49:30 -0700

    Fix nginx permission issues - run startup script as root, then switch to nginx user

--- client/start.sh
+++ client/start.sh
@@ -6,5 +6,9 @@ API_URL=${API_URL:-"http://api-qa-qa.up.railway.app"}
 # Replace the API URL in nginx config
 sed -i "s|http://api-qa-qa.up.railway.app|$API_URL|g" /etc/nginx/nginx.conf
 
-# Start nginx
-nginx -g 'daemon off;'
+# Create nginx pid directory and set permissions
+mkdir -p /var/run/nginx
+chown -R nginx:nginx /var/run/nginx
+
+# Start nginx as nginx user
+exec su-exec nginx nginx -g 'daemon off;'
